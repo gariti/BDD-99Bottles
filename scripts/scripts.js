91d3f66eccf7f1449eaf31c8ae30f9f542446964
Garrett Carver <gariti@gmail.com>
2015-10-22 18:30:11 -0700

    add clarifying comments in the code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,8 @@
 function bottlesOfBeer(numOfBottles) {
   var lyrics = "";
   var s = "s";
+
+  //the last part of the song has a few unique properties, so they get thier own variables to help simplify the logic
   var lastBottleLyric = "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. "
   var noBottlesLyric = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
 
