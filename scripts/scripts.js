a85efc4a1a542ed3bdbb53f799b7df9d957ffde6
Garrett Carver <gariti@gmail.com>
2015-10-22 17:47:56 -0700

    add dynamic lyrics for when function is given 2 or more bottles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,20 @@
 function bottlesOfBeer(numOfBottles) {
-  var oneBottleLyric = "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. "
-  var finalLyric = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+  var lyrics = "";
+  var lastBottleLyric = "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. "
+  var noBottlesLyric = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
 
-  if (numOfBottles > 0) {
-    return oneBottleLyric + finalLyric;
+  //build lyrics dynamically when 'numOfBottles' is 2 or greater
+  for (var i = numOfBottles; i > 1; i--) { 
+    lyrics = lyrics + i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall. "
+  }
+
+
+  //determine output of function
+  if (numOfBottles > 1) {
+    return lyrics + lastBottleLyric + noBottlesLyric;
+  } else if (numOfBottles === 1) {
+    return lastBottleLyric + noBottlesLyric;
   } else {
-    return finalLyric;
+    return noBottlesLyric;
   }
 }
--- spec/spec.js
+++ spec/spec.js
@@ -5,5 +5,7 @@ describe('bottlesOfBeer', function() {
   it("will return 1 lyric, then the final lyric if 1 bottle is given", function() {
     expect(bottlesOfBeer(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   });
-
+  it("will return 2 lyrics, then the final lyric if 2 bottles are given", function() {
+    expect(bottlesOfBeer(2)).to.equal("2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottles of beer on the wall. 1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  });
 });
