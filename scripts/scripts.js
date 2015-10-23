3075c3dec0e690d756894297fc7de0d3475ce306
Garrett Carver <gariti@gmail.com>
2015-10-22 18:15:02 -0700

    add spec for 5 bottles, fix words coming out with wrong plurality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,16 @@
 function bottlesOfBeer(numOfBottles) {
   var lyrics = "";
+  var s = "s";
   var lastBottleLyric = "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. "
   var noBottlesLyric = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
 
   //build lyrics dynamically when 'numOfBottles' is 2 or greater
   for (var i = numOfBottles; i > 1; i--) { 
-    lyrics = lyrics + i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall. "
+    if (i === 2) {s = ""} //used in the last dynamic lyric, correct the word 'bottles' to 'bottle'
+    lyrics = lyrics + i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around, " + (i - 1) + " bottle" + s + " of beer on the wall. "
   }
 
-
-  //determine output of function
+  //determine output of function depending on number of bottles passed
   if (numOfBottles > 1) {
     return lyrics + lastBottleLyric + noBottlesLyric;
   } else if (numOfBottles === 1) {
--- spec/spec.js
+++ spec/spec.js
@@ -6,6 +6,10 @@ describe('bottlesOfBeer', function() {
     expect(bottlesOfBeer(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   });
   it("will return 2 lyrics, then the final lyric if 2 bottles are given", function() {
-    expect(bottlesOfBeer(2)).to.equal("2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottles of beer on the wall. 1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+    expect(bottlesOfBeer(2)).to.equal("2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall. 1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   });
+  it("will return 5 lyrics, then the final lyric if 5 bottles are given", function() {
+    expect(bottlesOfBeer(5)).to.equal("5 bottles of beer on the wall, 5 bottles of beer. Take one down and pass it around, 4 bottles of beer on the wall. 4 bottles of beer on the wall, 4 bottles of beer. Take one down and pass it around, 3 bottles of beer on the wall. 3 bottles of beer on the wall, 3 bottles of beer. Take one down and pass it around, 2 bottles of beer on the wall. 2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall. 1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  });
+
 });
