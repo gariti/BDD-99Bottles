c870169de2fe009db5fc19020264914a9c626a7a
Garrett Carver <gariti@gmail.com>
2015-10-22 17:31:05 -0700

    have function work properly when 1 bottle is given

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,10 @@
-function bottlesOfBeer() {
+function bottlesOfBeer(numOfBottles) {
+  var oneBottleLyric = "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. "
   var finalLyric = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
-  return finalLyric
+
+  if (numOfBottles > 0) {
+    return oneBottleLyric + finalLyric;
+  } else {
+    return finalLyric;
+  }
 }
--- spec/spec.js
+++ spec/spec.js
@@ -2,4 +2,8 @@ describe('bottlesOfBeer', function() {
   it("will return only the final part of the song if 0 bottles are given", function() {
     expect(bottlesOfBeer(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   });
+  it("will return 1 lyric, then the final lyric if 1 bottle is given", function() {
+    expect(bottlesOfBeer(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, No more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  });
+
 });
